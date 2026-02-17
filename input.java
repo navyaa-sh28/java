400af717a22f50df80d0233652d28e00806de80c
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-02-17 09:34:54 +0530

    Added matrix addition program and input files

---
+++ input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class input {
+public static void main(String args[]){
+    Scanner input=new Scanner(System.in);
+    System.out.println("enter an integer");
+    int number=input.nextInt();
+}    
+}
